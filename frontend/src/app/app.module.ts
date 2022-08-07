919562576d4c984de6024e946b7a1e134434fe6a
MacielSevilha <dev.macielsevilha@gmail.com>
2022-08-07 18:51:06 -0300

    erro na routerLink

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,18 +8,32 @@ import { FooterComponent } from './components/template/footer/footer.component';
 import { NavbarComponent } from './components/template/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+
+import { MatCardModule } from '@angular/material/card'
+import { MatListModule } from '@angular/material/list';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
     NavbarComponent,
+    ProductReadComponent,
+    HomeComponent,
+    ProductCrudComponent,
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
